721b7035703946dab501c29b249c7dedf93bbd01
Ryan Scoville <ryanscoville29@yahoo.com>
2022-09-11 21:58:25 -0500

    refactoring of app

--- server/app.js
+++ server/app.js
@@ -12,7 +12,11 @@ app.get('/', (req, res)=> res.sendFile(path.join(__dirname, '..', 'public/index.
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
-app.use((req, res, next) => {
+  app.use('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public/index.html'));
+  });
+
+  app.use((req, res, next) => {
     if (path.extname(req.path).length) {
       const err = new Error('Not found')
       err.status = 404
